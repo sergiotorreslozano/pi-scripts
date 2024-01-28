793bd042fe389e09e23ac669c0f3a2fd2983ed94
sergiotorreslozano <sergiotorreslozano@gmail.com>
2024-01-28 18:26:00 +0100

    Changed clean_up to accept a path as parameter

--- src/clean_up.sh
+++ src/clean_up.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
+
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <file_path>"
+    exit 1
+fi
+
 # Define the file name
-client_file="client.log"
-server_file="server.log"
+file_path="$1"
 
 get_timestamp() {
     date +"%Y-%m-%d %H:%M:%S"
 }
 
 # Check if the file exists
-if [ -e "$client_file" ]; then
+if [ -e "$file_path" ]; then
     # File exists, so delete it
-    rm "$client_file"
+    rm "$file_path"
     echo "$(get_timestamp): File $client_file deleted, creating an empty one"
-    touch "$client_file"
+    touch "$file_path"
 else
     # File doesn't exist
     echo "$(get_timestamp): File $client_file not found, creating one empty"
-    touch "$client_file"
-fi
-
-
-# Check if the file exists
-if [ -e "$server_file" ]; then
-    # File server_file, so delete it
-    rm "$server_file"
-    echo "$(get_timestamp): File $server_file deleted, creating an empty one"
-    touch "$server_file"
-else
-    # File doesn't exist
-    echo "$(get_timestamp): File $server_file not found, creating one empty"
-    touch "$server_file"
+    touch "$file_path"
 fi
